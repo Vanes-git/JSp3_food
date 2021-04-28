45fcfd67e27048a31f340ed9afb795f9a83afaf1
Vanes <vanes.sysoev@gmail.com>
2021-04-28 17:42:26 +0300

    Refact: scripts
    divided main sript into modules and webpack to bundle them together

---
+++ js/modules/modal.js
@@ -0,0 +1,47 @@
+function modal(){
+    //Modal
+
+	const modalTrigger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal');
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modal.addEventListener('click', e => {
+        if (e.target === modal || e.target.getAttribute('data-close') == "") {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', e => {
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalBodyScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+        openModal();
+        window.removeEventListener('scroll', showModalBodyScroll);
+    }
+    }
+
+    window.addEventListener('scroll', showModalBodyScroll);
+}
+
+module.exports = modal;
